0135a4ff928606933b7fc257622cd34cb445c330
LOZORD <ljrudberg@gmail.com>
2016-02-28 21:03:53 -0600

    Started new version of the site

---
+++ new-sec-cam/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name seccamApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the seccamApp
+ */
+angular.module('seccamApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
